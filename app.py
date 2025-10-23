4c0051485cec55bc21586bbc2833cec01520a45b
pauleharris <paul@middlemiddleware.com>
2025-10-23 17:36:18 +0800

    fix SQLAlchemy version

--- app.py
+++ app.py
@@ -58,12 +58,9 @@ def update_subscription(token):
     
     return render_template('success.html', message=message)
 
-# Initialize database
-@app.before_first_request
-def create_tables():
+# Initialize database on startup
+with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
